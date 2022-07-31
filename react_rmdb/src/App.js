c77f2b4d9ca20767c8660b39ead2a8e512d3b0fc
Varian Lee <84647011+virtual-lee@users.noreply.github.com>
2022-07-31 21:02:53 +1000

    update

--- react_rmdb/src/App.js
+++ react_rmdb/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
 const App = () => {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ react_rmdb/src/GlobalStyle.js
@@ -0,0 +1,40 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --maxWidth: 1280px;
+        --white: #fff;
+        --lightGrey: #eee;
+        --darkGrey: #1c1c1c;
+        --fontSuperBig: 2.5rem;
+        --fontBig: 1.5rem;
+        --fontMed: 1.2rem;
+        --fontSmall: 1rem;
+    }
+
+    * {
+        box-sizing: border-box;
+        font-family: 'Abel', sans-serif;        
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+
+        h1 {
+            font-size: 2rem;
+            font-weight: 600;
+            color: var(--white);
+        }
+
+        h3 {
+            font-size: 1.1rem;
+        }
+
+        p {
+            font-size: 1rem;
+            color: var(--white);
+        }
+    }
+
+`;
