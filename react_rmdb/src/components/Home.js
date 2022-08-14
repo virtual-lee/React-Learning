1dc1f89b946b0c94fe1bc3b3a59fbc88c1618933
Varian Lee <84647011+virtual-lee@users.noreply.github.com>
2022-08-14 21:46:16 +1000

    update

--- react_rmdb/src/components/Home.js
+++ react_rmdb/src/components/Home.js
@@ -9,15 +9,19 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch' 
 //Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch();
     
     console.log(state)
+
+if (error) return <div>Something went wrong...</div>;
+
     return (
         <>
             {!searchTerm && state.results[0] ?
@@ -43,7 +47,10 @@ const Home = () => {
                         />
                 ))}
             </Grid>
-            <Spinner/>
+            {loading && <Spinner/>}
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
+            )}
         </>
         
     )
--- react_rmdb/src/hooks/useHomeFetch.js
+++ react_rmdb/src/hooks/useHomeFetch.js
@@ -14,6 +14,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
@@ -39,6 +40,15 @@ export const useHomeFetch = () => {
         setState(initialState);
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
+
+    //Load more
+    useEffect(() => {
+        if (!isLoadingMore) return;
+
+        fetchMovies(state.page +1, searchTerm);
+        setIsLoadingMore(false);
+
+    }, [isLoadingMore, searchTerm, state.page])
     
-    return { state, loading, error, searchTerm, setSearchTerm };
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
